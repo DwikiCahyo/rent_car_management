0794dc8821dd2b32e75d351472d1ce6bd66273c0
DwikiCahyo <cbudi35@gmail.com>
2023-12-15 14:03:35 +0700

    fix:app.js

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(express.static("views"));
 app.set("view engine", "ejs");
 const port = process.env.PORT || 3002;
 
+console.log(process.env.NODE_ENV);
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/v1/", router);
